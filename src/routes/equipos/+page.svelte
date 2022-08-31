e5f227ef2f2ce39a606f0bb41daaa6f1556d49ae
matiaguilera <73367049+matiaguilera@users.noreply.github.com>
2022-08-31 18:14:18 -0400

    Add team logos

--- src/routes/equipos/+page.svelte
+++ src/routes/equipos/+page.svelte
@@ -7,8 +7,8 @@
 
 <div class="py-8 w-full">
 	<div class="md:flex flex-wrap items-center justify-center">
-		{#each teams as team}
-			<TeamItem name={team.name} tid={team.id} />
+		{#each teams as { id: tid, name, code }}
+			<TeamItem {tid} {name} {code} />
 		{/each}
 	</div>
 </div>
--- src/routes/equipos/TeamItem.svelte
+++ src/routes/equipos/TeamItem.svelte
@@ -1,18 +1,23 @@
 <script lang="ts">
 	import LinkButton from '$lib/components/LinkButton.svelte';
 
-	export let tid: number, name: string;
+	export let tid: number, name: string, code: string;
 </script>
 
 <div class="md:w-4/12 md:mx-7 mb-7 p-6 shadow rounded">
 	<div class="flex items-center border-b border-gray-200 pb-6">
-		<img class="rounded-full" src="/team.png" alt="Team logo" width={48} height={48} />
+		<img
+			class="mx-3"
+			src={code !== 'ESP' ? `/${code}.webp` : `/${code}.jpg`}
+			alt="Team logo"
+			width={48}
+			height={48}
+		/>
 		<div class="flex items-start justify-between w-full">
 			<div class="pl-3 w-full">
 				<p class="text-xl font-medium leading-5 text-gray-800">
 					{name}
 				</p>
-				<p class="text-sm leading-normal pt-2 text-gray-500">22 jugadores</p>
 			</div>
 		</div>
 	</div>
