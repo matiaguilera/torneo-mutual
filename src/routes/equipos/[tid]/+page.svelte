af8a11616379c42e4e7bc5c1d785f85a8c2cac66
matiaguilera <73367049+matiaguilera@users.noreply.github.com>
2022-09-04 18:25:41 -0400

    Minor improvements

--- src/routes/equipos/[tid]/+page.svelte
+++ src/routes/equipos/[tid]/+page.svelte
@@ -6,10 +6,16 @@
 	let { players } = data;
 </script>
 
-<div class="py-8 w-full">
-	<div class="md:flex flex-wrap items-center justify-center w-full">
-		{#each players as { name, lastName, number }}
-			<PlayerItem {name} {lastName} {number} />
-		{/each}
+{#if players.length > 0}
+	<div class="py-8 w-full">
+		<div class="md:flex flex-wrap items-center justify-center w-full">
+			{#each players as { name, lastName, number }}
+				<PlayerItem {name} {lastName} {number} />
+			{/each}
+		</div>
 	</div>
-</div>
+{:else}
+	<div class="flex items-center justify-center mt-10">
+		<p class="text-gray-600">Próximamente</p>
+	</div>
+{/if}
