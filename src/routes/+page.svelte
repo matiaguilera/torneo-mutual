9d486c12453601e36d9e8a984e1881b7b3fc671b
matiaguilera <73367049+matiaguilera@users.noreply.github.com>
2022-09-10 11:37:17 -0400

    Develop landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,60 @@
-<script lang="ts">
-	import Information from './Information.svelte';
-
-	export let data;
-
-	let { leagueData } = data;
-</script>
-
-<Information {leagueData} />
+<div class="relative overflow-hidden shadow">
+	<div class="mx-auto max-w-7xl">
+		<div class="relative z-10 pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-32">
+			<svg
+				class="absolute -z-10 inset-y-0 right-0 hidden h-full w-48 translate-x-1/2 transform text-white lg:block"
+				fill="currentColor"
+				viewBox="0 0 100 100"
+				preserveAspectRatio="none"
+				aria-hidden="true"
+			>
+				<polygon points="0,0 100,0 50,100 0,100" />
+			</svg>
+			<main class="mx-auto sm:pt-10 pt-5 max-w-7xl px-4 sm:px-6">
+				<div class="sm:text-center lg:text-left">
+					<h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+						<span class="block xl:inline">Bienvenido/a</span>
+					</h1>
+					<p
+						class="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0"
+					>
+						Aquí encontrarás información como fechas, tabla de posiciones, goleadores y más. Todo
+						actualizado en tiempo real.
+					</p>
+					<div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+						<div class="rounded-md shadow">
+							<a
+								href="/"
+								class="flex w-full items-center justify-center rounded-md border border-transparent bg-slate-600 px-8 py-3 text-base font-medium text-white hover:bg-slate-700 md:py-4 md:px-10 md:text-lg"
+								>Reglamento</a
+							>
+						</div>
+						<div class="mt-3 sm:mt-0 sm:ml-3">
+							<a
+								href="/"
+								class="flex w-full items-center justify-center rounded-md border border-transparent bg-slate-100 px-8 py-3 text-base font-medium text-slate-700 hover:bg-slate-200 md:py-4 md:px-10 md:text-lg"
+								>Próximos partidos</a
+							>
+						</div>
+					</div>
+				</div>
+			</main>
+		</div>
+	</div>
+	<svg
+		class="absolute -z-10 inset-y-0 right-0 hidden h-full w-48 translate-x-1/2 transform text-white lg:block"
+		fill="currentColor"
+		viewBox="0 0 100 100"
+		preserveAspectRatio="none"
+		aria-hidden="true"
+	>
+		<polygon points="0,0 100,0 50,100 0,100" />
+	</svg>
+	<div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:-z-10">
+		<img
+			class="h-56 w-full object-cover sm:h-72 md:h-96 lg:h-full lg:w-full"
+			src="https://cdn.wallpapersafari.com/53/12/uOJBk5.jpg"
+			alt=""
+		/>
+	</div>
+</div>
