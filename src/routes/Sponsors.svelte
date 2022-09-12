83db687998bfd83a5b466d5299f5da763d6840fb
matiaguilera <73367049+matiaguilera@users.noreply.github.com>
2022-09-12 19:33:45 -0400

    Add sponsors

--- src/routes/Sponsors.svelte
+++ src/routes/Sponsors.svelte
@@ -1,4 +1,4 @@
-<div class="flex justify-center flex-wrap overflow-auto bg-slate-100">
+<!-- <div class="flex justify-center flex-wrap overflow-auto bg-slate-100">
 	<div
 		class="m-5 h-24 flex w-44 shrink-0 items-center justify-center rounded-lg border-4 border-dotted border-slate-400"
 	>
@@ -19,4 +19,17 @@
 	>
 		<div class="font-thin">Nombre o logo</div>
 	</div>
+</div> -->
+
+<div class="flex justify-center flex-wrap overflow-auto bg-white">
+	<div class="flex items-center py-4">
+		<img class="m-5 w-44 object-scale-down" src="/sponsors/JK.jpg" alt="JK" />
+	</div>
+	<div class="flex items-center py-4">
+		<img class="m-5 w-44 object-scale-down" src="/sponsors/4RUEDAS.jpg" alt="4 ruedas" />
+	</div>
+	<div class="px-8">
+		<img class="m-5 w-28 object-scale-down" src="/sponsors/AUTENTIC.jpg" alt="Autentic" />
+	</div>
+	<img class="m-5 w-44 object-scale-down" src="/sponsors/INGENIERIA.jpg" alt="Ingeniería" />
 </div>
