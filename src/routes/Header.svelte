026d73bd110dec66fcf7f14f544ab5a1ee94491a
matiaguilera <73367049+matiaguilera@users.noreply.github.com>
2022-09-17 18:56:16 -0400

    Add facebook link

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,8 +1,8 @@
 <div class="bg-slate-900 h-16 flex items-center justify-between px-4 sm:px-6 lg:px-8">
 	<a href="/">
-		<div class="flex-shrink-0 mt-1 font-medium text-slate-200">Torneo Mutual de Franco</div>
+		<div class="flex-shrink-0 mt-1 font-medium text-slate-200">AEFAF Copa Mundial</div>
 	</a>
-	<a target="_blank" href="https://facebook.com">
+	<a target="_blank" href="https://www.facebook.com/groups/1386329371449040">
 		<svg
 			width="20"
 			height="20"
