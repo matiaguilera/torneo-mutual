c8e70cc6259752ff398aa930dae4e51591b6f060
matiaguilera <73367049+matiaguilera@users.noreply.github.com>
2022-09-17 18:56:48 -0400

    Delete sponsor

--- src/routes/Sponsors.svelte
+++ src/routes/Sponsors.svelte
@@ -1,31 +1,8 @@
-<!-- <div class="flex justify-center flex-wrap overflow-auto bg-slate-100">
-	<div
-		class="m-5 h-24 flex w-44 shrink-0 items-center justify-center rounded-lg border-4 border-dotted border-slate-400"
-	>
-		<div class="font-thin">Nombre o logo</div>
-	</div>
-	<div
-		class="m-5 h-24 flex w-44 shrink-0 items-center justify-center rounded-lg border-4 border-dotted border-slate-400"
-	>
-		<div class="font-thin">Nombre o logo</div>
-	</div>
-	<div
-		class="m-5 h-24 flex w-44 shrink-0 items-center justify-center rounded-lg border-4 border-dotted border-slate-400"
-	>
-		<div class="font-thin">Nombre o logo</div>
-	</div>
-	<div
-		class="m-5 h-24 flex w-44 shrink-0 items-center justify-center rounded-lg border-4 border-dotted border-slate-400"
-	>
-		<div class="font-thin">Nombre o logo</div>
-	</div>
-</div> -->
-
 <div class="flex justify-center flex-wrap overflow-auto bg-white">
 	<div class="font-extrabold w-44 m-5 text-center flex items-center">MN IMPORT. EXPORT. S.R.L</div>
-	<div class="flex items-center py-4">
+	<!-- <div class="flex items-center py-4">
 		<img class="m-5 w-44 object-scale-down" src="/sponsors/JK.jpg" alt="JK" />
-	</div>
+	</div> -->
 	<div class="flex items-center py-4">
 		<img class="m-5 w-44 object-scale-down" src="/sponsors/4RUEDAS.jpg" alt="4 ruedas" />
 	</div>
