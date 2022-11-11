f1cc364e74dfaf2e70aa70c6c2b7fce5099de7ca
matiaguilera <73367049+matiaguilera@users.noreply.github.com>
2022-11-10 22:05:39 -0300

    Add support for final matches

--- src/routes/finales/+page.svelte
+++ src/routes/finales/+page.svelte
@@ -5,15 +5,24 @@
 	let { matches } = data;
 </script>
 
-<h3 class="text-gray-600 font-medium text-lg text-center mt-4">Semifinales</h3>
 <div class="flex justify-center my-5">
 	<div class="grid grid-cols-1 md:grid-cols-2 gap-4">
 		{#each matches as { id, home, away, matchday, date, status, goals }, index}
 			{#if index === 0}
+				<h3 class="col-span-2 text-gray-600 font-medium text-lg text-center mt-2">Semifinales</h3>
 				<div class="md:col-span-2 bg-slate-100 border-y">
 					<p class="text-black pl-2">Primera vuelta</p>
 				</div>
 			{:else if index === 4}
+				<div class="md:col-span-2 bg-slate-100 border-y">
+					<p class="text-black pl-2">Segunda vuelta</p>
+				</div>
+			{:else if index === 8}
+				<h3 class="col-span-2 text-gray-600 font-medium text-lg text-center mt-4">Finales</h3>
+				<div class="md:col-span-2 bg-slate-100 border-y">
+					<p class="text-black pl-2">Primera vuelta</p>
+				</div>
+			{:else if index === 10}
 				<div class="md:col-span-2 bg-slate-100 border-y">
 					<p class="text-black pl-2">Segunda vuelta</p>
 				</div>
